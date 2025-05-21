831ab4db6755f462732283e165a4976722410e1d
Ishitvaa <ishitvamahajan29@gmail.com>
2025-05-21 12:21:13 +0530

    set-up of project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// import PORT 
+const { PORT } = require('./config/serverConfig.js'); 
+
+const setUpAndStartServer = async () => {
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on Port ${PORT}`);
+        // console.log(process.env);
+    });
+
+};
+
+setUpAndStartServer();
