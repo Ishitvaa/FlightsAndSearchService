b6c1754aa146a4420f611dd8f1d04e684ac9430f
Ishitvaa <ishitvamahajan29@gmail.com>
2025-05-23 12:57:54 +0530

    completed city-controller

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,1 +1,97 @@
-const { cityService } = require('../services/index.js');
+const { CityService } = require('../services/index.js');
+
+const cityService = new CityService();
+
+const create = async(req, res) => {
+    try {
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data : city,
+            status : true,
+            message : 'Successfully created a city',
+            error : {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data : {},
+            status : false,
+            message : 'Not able to create city',
+            error : error
+        });
+    }
+}
+
+// GET -> /city/:id
+const get = async(req, res) => {
+    try {
+        const city = await cityService.getCity(req.params.cityId);
+        return res.status(201).json({
+            data : city,
+            status: true,
+            message : 'successfully fetched city',
+            error : {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data : {},
+            status : false,
+            message : 'Not able to fetch city',
+            error : error
+        });
+    }
+}
+
+// Patch -> /city/:id -> data(req.body)
+const update= async(req, res) => {
+    try {
+        const city = await cityService.updateCity(req.params.cityId, req.body);
+        return res.status(201).json({
+            data : city,
+            status : true,
+            message : 'Successfully updated city',
+            error : {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data : {},
+            status : false,
+            message : 'Not able to update city',
+            error : error
+        });
+    }
+}
+
+// delete -> /city/:id
+const destroy = async(req, res) => {
+    try {
+        const response = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data : response,
+            status : true,
+            message : 'Successfully deleted a city',
+            error : {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data : {},
+            status : false,
+            message : 'Not able to update city',
+            error : error
+        });
+    }
+}
+
+module.exports = {
+    create,
+    get,
+    destroy,
+    update
+}
+
+// work flow : controller -> service -> repository 
+// service and repository apna kaam kr ke controller ko response bhejenge
+// then controller us response ko format kr ke return kr dega
