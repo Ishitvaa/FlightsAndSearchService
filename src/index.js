fdcf7f9186e9e83501c6c59d7d20e1883631b1a5
Ishitvaa <ishitvamahajan29@gmail.com>
2025-05-26 11:44:46 +0530

    Added model of flights and airplanes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const ApiRoutes = require('./routes/index.js');
 // import PORT 
 const { PORT } = require('./config/serverConfig.js'); 
 
-const { Airport, City } = require('./models/index.js');
+const sequelize = require('sequelize');
+
+const { Airport, City , Airplane} = require('./models/index.js');
+const db = require('./models/index.js');
 
 const setUpAndStartServer = async () => {
 
@@ -15,22 +18,42 @@ const setUpAndStartServer = async () => {
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is listening on Port ${PORT}`);
-        // console.log(process.env);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter : true});
+        }
+
+        // include -> to associate with other models
+        // const airport = await City.findAll({
+        //     // where : {
+        //     //     id : 11
+        //     // },
+        //     include : [{
+        //         model : Airport
+        //     }]
+        // })
+
+        // select city of particular cityId:
+        // const city = await City.findOne({
+        //     where : {
+        //         id : 11
+        //     }
+        // });
+        // // can get all airports of the cities:
+        // const airports = await city.getAirports();
+
+        // to add airport manually to a particular city using cityId:
+        // await city.addAirports({
+            
+        // })
+
+        // console.log(city, airports);
+
+        // await Airplane.create({
+        //     modelNumber: 'Bombardier CRJ'
+        // })
     });
-
-    // include -> 
-    const airport = await City.findAll({
-        // where : {
-        //     id : 11
-        // },
-        include : [{
-            model : Airport
-        }]
-    })
-
-    console.log(airport);
 
 };
 
